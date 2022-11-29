45d93a3c85135b3c839741dfc30aa260495f8caf
Justin <info@justinlmartin.com>
2022-11-28 21:15:06 -0800

    added some css

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ function makeLinks(mobile) {
     const links = linksData.filter(link => link.show)
     let container = document.getElementById('main')
     links.forEach(link => {
-        let id = link.id.concat('link')
+        let id = link.id.concat('-link')
         let div = document.createElement('div')
         let linkTag = document.createElement('a')
         div.setAttribute('id', id)
         div.classList.add('link-container')
         linkTag.setAttribute('href', (mobile) ? link.mobileLink : link.link,)
         linkTag.setAttribute('target', '_blank')
+        linkTag.classList.add('link')
         linkTag.innerHTML = link.text
         div.appendChild(linkTag)
         container.appendChild(div)
